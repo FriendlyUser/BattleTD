c11ee9c80d5c5a376b1326bf1f5e0b95cf71baae
dli <garbage@gmail.com>
2018-10-16 19:02:21 -0700

    Revert "adding stuff"
    This reverts commit ca4484dd6846cc6e26cceb69e602ee40fd20e4a7.

---
+++ Travis/deploy.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+SOURCE_BRANCH="master"
+TARGET_BRANCH="azure-manual"
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    doCompile
+    exit 0
+fi
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+# Clone the existing gh-pages for this repo into out/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deploy)
+rm -rf .gitignore
+git clone $REPO out
+cd out
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cd ..
+
+# Clean out existing contents
+rm -rf out/**/* || exit 0
+
+# Copy webgl to directory 
+ls 
+cp -R Build/webgl/ out/
+
+# Now let's go have some fun with the cloned repo
+cd out
+echo "About to deploy"
+ls
+git config user.name "TRAVISCI BOT"
+git config user.email "studentdavidli@gmail.com"
+
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
+
+# Add large file system for unity web output files
+git lfs track "*.unityweb"
+git add .gitattributes
+rm -rf travis.yml
+
+git add .
+git commit -m "Deploy to GitHub Pages: ${SHA} [ci skip]"
+
+git remote rm origin
+git remote add origin https://FriendlyUser:$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG 
+git push origin $TARGET_BRANCH --force
+
+echo "Complete File Set"
